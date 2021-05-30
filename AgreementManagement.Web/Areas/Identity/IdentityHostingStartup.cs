3da1d922b6dc0c73ebd5c37ebba8ff0120e92390
Aleksa Ristic <aleksa.ristic@tieto.com>
2021-05-30 16:18:35 +0200

    Initial commit: MVC template created, build-in Authentication included, some visual improvements

---
+++ AgreementManagement.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AgreementManagement.Web.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AgreementManagement.Web.Areas.Identity.IdentityHostingStartup))]
+namespace AgreementManagement.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
